908cd61caf15657017499a88925839e46fb0f28c
Munetaka Ohtani <ohtanim@jp.ibm.com>
2026-01-25 00:37:57 +0900

    Update job script dependency path

--- slurm/1_multiple_jobs/run.sh
+++ slurm/1_multiple_jobs/run.sh
@@ -1,2 +1,2 @@
 jobidA=$(sbatch run_sampler.sh | awk '{print $4}')
-sbatch --dependency=afterok:$jobidA /shared/job_scripts/mpi/get_counts/get_counts_job.sh
+sbatch --dependency=afterok:$jobidA get_counts_job.sh
